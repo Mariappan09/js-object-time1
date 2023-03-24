d711abb455cc8f575993de2797e98aa9a3f74297
Your Name <youamariappan2001@example.com>
2023-03-24 17:02:12 +0530

    add

---
+++ main.js
@@ -0,0 +1,23 @@
+let a=new Date();
+	let hour=a.getHours();
+	if(hour>=6 && hour<=11)
+	{
+		console.log("morning");
+	}
+	else if(hour>=12 && hour<=16)
+	{
+		console.log("afternoun");
+	}
+	else
+	{
+		console.log("night");
+	}
+	
+
+	
+	
+	
+	
+	
+	
+	
